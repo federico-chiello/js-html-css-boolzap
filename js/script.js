a91667476076cf95efd294504a42d9f7c32d6242
Fede <chiellofederico@gmail.com>
2021-02-02 10:00:36 +0100

    inserimento script.js

---
+++ js/script.js
@@ -0,0 +1,3 @@
+// Milestone 1
+// ● Replica della grafica c on l a possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse.
+// ● Visualizzazione dinamica della lista contatti: tramite l a direttiva v-for, visualizzare nome e immagine di ogni contatto.
