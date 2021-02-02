03a4fba63873846f737d83f1648f8867398d1b9c
Fede <chiellofederico@gmail.com>
2021-02-02 10:36:10 +0100

    array users

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 // Milestone 1
 // ● Replica della grafica c on l a possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse.
 // ● Visualizzazione dinamica della lista contatti: tramite l a direttiva v-for, visualizzare nome e immagine di ogni contatto.
+
+var app = new Vue ({
+  el: '#boolzap',
+  data: {
+    users: [
+      {
+        name: 'Sofia',
+        avatar: '_io'
+      }
+    ],
+    contacts: [],
+    adressee:[]
+  }
+});
